ab550f5da90704157f6d47fc13f53f05a223f428
Guillermo Jimenez Diaz <gjimenez@ucm.es>
2022-03-02 17:46:13 +0100

    Refactoring: encapsulation  of mongoose access if db change is needed

--- api-server/index.js
+++ api-server/index.js
@@ -40,27 +40,17 @@ async function run () {
     }
   });
 
-  const db = require("./models");
 
-  console.log(db.url);
+
+ const db = require("./models");
+ db.init();
   
-  db.mongoose
-    .connect(db.url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    .then(() => {
-      console.log("Connected to the database!");
-    })
-    .catch(err => {
-      console.log("Cannot connect to the database!", err);
-      process.exit();
-    });
+
 
 
 
   
-  const PORT = process.env.NODE_DOCKER_PORT || 3000;
+  const PORT = 3000;
   app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}.`);
   });
--- api-server/models/index.js
+++ api-server/models/index.js
@@ -1,11 +1,28 @@
 const dbConfig = require("../config/db.config.js");
 
 const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
 db.communities = require("./community.model.js")(mongoose);
 
-module.exports = db;
+
+module.exports = {
+    init: function() {
+        console.log(db.url);
+        db.mongoose
+        .connect(db.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => {
+            console.log("Connected to the database!");
+        })
+        .catch(err => {
+            console.log("Cannot connect to the database!", err);
+            process.exit();
+        });
+    },
+    communities: db.communities
+};
