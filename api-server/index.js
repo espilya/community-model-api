a204c82270c4e520d8ce960713b00d4b6974a8c6
Guillermo Jimenez Diaz <gjimenez@ucm.es>
2022-03-03 13:19:56 +0100

    Refactoring for the creation of a test suite

--- api-server/index.js
+++ api-server/index.js
@@ -5,29 +5,24 @@ require("dotenv").config();
 
 const apiyaml = "./app/api/openapi.yaml";
 
-
-
-async function run () {
+async function initServer() {
   const app = express();
-
   app.use(express.json()); // for parsing application/json
   app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-  
+
   // Any paths defined in your openapi.yml will validate and parse the request
   // before it calls your route code.
   const enforcerMiddleware = EnforcerMiddleware(await Enforcer(apiyaml));
   app.use(enforcerMiddleware.init());
-  
+
   // Catch errors
   enforcerMiddleware.on('error', err => {
     console.error(err);
     process.exit(1);
-  }); 
+  });
 
   app.set("enforcer", enforcerMiddleware);
   require("./app/routes/routes.js")(app);
-
-
 
   app.use((err, req, res, next) => {
     if (err.statusCode >= 400 && err.statusCode < 500 && err.exception) {
@@ -39,17 +34,17 @@ async function run () {
       res.sendStatus(err.statusCode || 500);
     }
   });
+  return app;
+}
 
+function initDatabaseConnection() {
+  const db = require("./app/models");
+  db.init();
+}
 
-
- const db = require("./app/models");
- db.init();
-  
-
-
-
-
-  
+async function run () {
+  const app = await initServer();
+  initDatabaseConnection();
   const PORT = process.env.NODE_DOCKER_PORT || 3000;
   app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}.`);
@@ -57,3 +52,4 @@ async function run () {
 }
 
 run().catch(console.error);
+
