87d7a65732006f5388e6f9706e59e5e2aeb17524
Guillermo Jimenez Diaz <gjimenez@ucm.es>
2022-03-04 11:54:43 +0100

    Testing Created a test suite for API (WIP)

--- api-server/test/api.test.js
+++ api-server/test/api.test.js
@@ -17,17 +17,118 @@ beforeAll(()=>{
 });
 
 
-describe ("Test example", ()=>{
+describe ("Test communities endpoint", ()=>{
     test ("GET /communities", (done)=>{
         request(app)
             .get('/communities')
             .expect('Content-Type', /json/)
             .expect(200)
             .end(function(err, res) {
+                if (err) return done(err);
+                return done();
+            });
+    });
+    test ("GET /communities/{id}/users", (done)=>{
+        request(app)
+            .get('/communities/621e53cf0aa6aa7517c2afdd/users')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                return done();
+            });
+    });
+    test ("GET /communities/BAD_id/users", (done)=>{
+        request(app)
+            .get('/communities/wrongId/users')
+            .expect(400)
+            .end(function(err, res) {
                 if (err) throw err;
+                return done();
+            });
+    });
+    test ("GET /communities/{id}", (done)=>{
+        request(app)
+            .get('/communities/621e53cf0aa6aa7517c2afdd/users')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                return done();
+            });
+    });
+    test ("GET /communities/BAD_id", (done)=>{
+        request(app)
+            .get('/communities/wrongId')
+            .expect(400)
+            .end(function(err, res) {
+                if (err) return done(err);
                 return done();
             });
     });
 });
 
+describe ("Test users endpoint", ()=>{
+    test ("GET /users/{user-id}/communities", (done)=>{
+        request(app)
+            .get('/users/821e53cf0aa6aa7517c2afdd/communities')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                return done();
+            });
+    });
+    // test ("POST /users/user-generated-content", (done)=>{
+    //     request(app)
+    //         .post('/users/user-generated-content')
+    //         .expect(204)
+    //         .end(function(err, res) {
+        // if (err) return done(err);
+        // return done();
+    //         });
+    // });
+    // test ("GET /users/{BADUSERID}/communities", (done)=>{
+    //     request(app)
+    //         .get('/communities/wrongId/users')
+    //         .expect(400)
+    //         .end(function(err, res) {
+        // if (err) return done(err);
+        // return done();
+    //         });
+    // });
+});
+
+describe ("Test similarity endpoint", ()=>{
+    test ("GET /communities/{community-id}/similarity", (done)=>{
+        request(app)
+            .get('/communities/821e53cf0aa6aa7517c2afdd/similarity')
+            .query({ k: '2' })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                return done();
+            });
+    });
+    test ("GET /communities/{community-id}/similarity NO QUERY PARAMETER", (done)=>{
+        request(app)
+            .get('/communities/821e53cf0aa6aa7517c2afdd/similarity')
+            .expect(400)
+            .end(function(err, res) {
+                if (err) return done(err);
+                return done();
+            });
+    });
+    test ("GET /communities/{BADCOMMUNITYID}/similarity", (done)=>{
+        request(app)
+            .get('/communities/badId/similarity')
+            .query({ k: '2' })
+            .expect(400)
+            .end(function(err, res) {
+                if (err) return done(err);
+                return done();
+            });
+    });
+});
 
